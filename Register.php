69038c9f1afdb19c6ee348c963947c730bd6ac13
Ibrahimt Tameme <ibrahimtameme98@gmail.com>
2022-05-06 21:30:21 +0300

    done

---
+++ Register.php
@@ -0,0 +1,279 @@
+<?php
+session_start();
+
+$name_regex="/^([a-zA-Z' ]+)$/";
+$email_regex="/^[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/";
+$password_regex = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/"; 
+
+
+if (isset($_POST['submit'])){
+$_SESSION['firstName']=$_POST['firstname'];
+$_SESSION['secondname']=$_POST['secondname'];
+$_SESSION['thirdname']=$_POST['thirdname'];
+$_SESSION['fourthname']=$_POST['fourthname'];
+$_SESSION['email']=$_POST['signUpEmail'];
+$_SESSION['password']=$_POST['signUpPassword'];
+$_SESSION['confirmPassword']=$_POST['signUpConfirmPassword'];
+
+$_SESSION['dateOfBirth']=$_POST['DOB'];
+$_SESSION['array']=array('');
+$_SESSION['date_create']=date("Y-m-d"); //Date Create
+    // First name check
+    if(preg_match($name_regex,$_SESSION['firstName'])){
+        $firstName_result="<span style=' color:green'>Correct Name</span> <br>";
+        $firstName_correct=true;
+    }else{
+        $firstName_result="<span style=' color:red'>InCorrect Name</span> <br>";
+        $firstName_correct=false;
+    }
+    //Middle name check
+    if(preg_match($name_regex,$_SESSION['secondname'])){
+        $secondname_result="<span style=' color:green'>Correct Name</span> <br>";
+        $secondName_correct=true;
+    }else{
+        $secondname_result="<span style=' color:red'>InCorrect Name</span> <br>";
+        $secondName_correct=false;
+    }
+       //Middle name check
+       if(preg_match($name_regex,$_SESSION['thirdname'])){
+        $thirdname_result="<span style=' color:green'>Correct Name</span> <br>";
+        $thirdname_correct=true;
+    }else{
+        $thirdname_result="<span style=' color:red'>InCorrect Name</span> <br>";
+        $thirdname_correct=false;
+    }
+    //Family Name
+    if(preg_match($name_regex,$_SESSION['fourthname'])){
+        $fourthname_result="<span style=' color:green'>Correct Name</span> <br>";
+        $fourthname_correct=true;
+    }else{
+        $fourthname_result="<span style=' color:red'>InCorrect Name</span> <br>";
+        $fourthname_correct=false;
+    }
+    //Email
+    if(preg_match($email_regex,$_SESSION['email'])){
+        $email_result="<span style=' color:green'>Correct Email</span> <br>";
+        $email_correct=true;
+    }
+    else{
+        $email_result="<span style=' color:red'>Incorrect Email</span> <br>";
+        $email_correct=false;
+    }
+    //Password
+    if(preg_match($password_regex,$_SESSION['password'])){
+        $password_result="<span style=' color:green'>Correct Password</span> <br>";
+        $password_correct=true;
+    }
+    else{
+        $password_result="<span style=' color:red'>Incorrect Password, your password shoud have:<br>1- 8 characters at least<br>2- At least one uppercase English letter<br>3- At least one lowercase English letter<br>4- At least one digit<br>5- At least one special character </span> <br>";
+        $paswword_correct=false;
+    }
+    //Confirm Password
+    if(preg_match($password_regex,$_SESSION['confirmPassword'])){
+        if ($_SESSION['confirmPassword'] == $_SESSION['password']){
+            $password_match=true;
+            $confirmPassword_correct=true;
+            $confirmPassword_result="<span style=' color:green'>Correct Password</span> <br>";
+        }
+        else{
+            $password_match=false;
+            $confirmPassword_result="<span style=' color:red'>Password doesn't match</span> <br>";
+        }
+        
+    }
+    else{
+        $confirmPassword_result="<span style=' color:red'>Incorrect Password, your password shoud have:<br>1- 8 characters at least<br>2- At least one uppercase English letter<br>3- At least one lowercase English letter<br>4- At least one digit<br>5- At least one special character </span> <br>";
+        $confirmPaswword_correct=false;
+    }
+    
+    //Date Of Birth
+    if((floor((time() - strtotime($_SESSION['dateOfBirth'])) / 31556926)) >16){
+        $dob_result="<span style=' color:green'>Your age is greater than 16</span> <br>";
+        $confirmDob_correct=true;
+    }
+    
+    else{
+        $dob_result="<span style=' color:red'>Your age is less than 16</span> <br>";
+        $confirmDob_correct=false;
+    }
+    if(
+        $firstName_correct && $secondName_correct && $thirdname_correct && $fourthname_correct && $email_correct && $confirmPassword_correct  && $confirmDob_correct
+    ){
+        $_SESSION['array']=array(
+            'First Name'=> $_SESSION['firstName'],
+            'second Name'=> $_SESSION['secondname'],
+            'Last Name'=>$_SESSION['thirdname'],
+            'Family Name'=> $_SESSION['fourthname'],
+            'Email'=> $_SESSION['email'],
+            'Password'=> $_SESSION['password'],
+            'Password Confirmation'=> $_SESSION['confirmPassword'],
+            
+            'Date Of Birth'=>$_SESSION['dateOfBirth']
+        );
+        
+        header('location:login.php');
+    }
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Register.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<form method="post" >
+    <section  class="h-100 bg-dark">
+        <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col">
+              <div class="card card-registration my-4">
+                <div class="row g-0">
+                  <div class="col-xl-6 d-none d-xl-block">
+                    <img src="./img/pexels-pixabay-33684.jpg"
+                      alt="Sample photo" class="imgg"
+                      style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem;" />
+                  </div>
+                  <div class="col-xl-6">
+                    <div class="card-body p-md-5 text-black">
+                      <h3 class="mb-5 text-uppercase">Student registration form</h3>
+      
+                      <div class="row">
+                        
+                        <div class="col-md-6 mb-4">
+                        <?php if(isset($firstName_result)){echo $firstName_result;}?>
+                          <div class="form-outline">
+                            <input type="text" name="firstname" id="form3Example1m" class="form-control form-control-lg" required />
+                            <label class="form-label" for="form3Example1m">First name</label>
+                            
+                          </div>
+                        </div>
+                        
+                        <div class="col-md-6 mb-4">
+                        <?php if(isset($secondname_result)){echo $secondname_result;}?>
+                          <div class="form-outline">
+                            <input type="text" name="secondname" id="form3Example1n" class="form-control form-control-lg" required />
+                            <label class="form-label" for="form3Example1n">middle name</label>
+                          </div>
+                        </div>
+                      </div>
+                      
+      
+                      <div class="row">
+                        <div class="col-md-6 mb-4">
+                        <?php if(isset($thirdname_result)){echo $thirdname_result;}?>
+                          <div class="form-outline">
+                            <input type="text" name="thirdname" id="form3Example1m1" class="form-control form-control-lg" required />
+                            <label class="form-label" for="form3Example1m1">last name</label>
+                          </div>
+                        </div>
+                        <div class="col-md-6 mb-4">
+                        <?php if(isset($fourthname_result)){echo $fourthname_result;}?>
+                          <div class="form-outline">
+                            <input type="text" name="fourthname" id="form3Example1n1" class="form-control form-control-lg"  required />
+                            <label class="form-label" for="form3Example1n1">family name</label>
+                          </div>
+                        </div>
+                      </div>
+      
+                      <div class="form-outline mb-4">
+                      <?php if(isset($email_result)){echo $email_result;}?>
+                        <input type="text" name="signUpEmail" id="form3Example8" class="form-control form-control-lg" required />
+                        <label class="form-label" for="form3Example8">E-mail</label>
+                      </div>
+      
+                      <!-- <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
+      
+                        <h6 class="mb-0 me-4">Gender: </h6>
+      
+                        <div class="form-check form-check-inline mb-0 me-4">
+                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
+                            value="option1" />
+                          <label class="form-check-label" for="femaleGender">Female</label>
+                        </div>
+      
+                        <div class="form-check form-check-inline mb-0 me-4">
+                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender"
+                            value="option2" />
+                          <label class="form-check-label" for="maleGender">Male</label>
+                        </div>
+      
+                        <div class="form-check form-check-inline mb-0">
+                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender"
+                            value="option3" />
+                          <label class="form-check-label" for="otherGender">Other</label>
+                        </div>
+      
+                      </div>
+       -->
+                      <!-- <div class="row">
+                        <div class="col-md-6 mb-4">
+      
+                          <select class="select">
+                            <option value="1">State</option>
+                            <option value="2">Option 1</option>
+                            <option value="3">Option 2</option>
+                            <option value="4">Option 3</option>
+                          </select>
+      
+                        </div>
+                        <div class="col-md-6 mb-4">
+      
+                          <select class="select">
+                            <option value="1">City</option>
+                            <option value="2">Option 1</option>
+                            <option value="3">Option 2</option>
+                            <option value="4">Option 3</option>
+                          </select>
+      
+                        </div>
+                      </div>
+       -->
+                      <div class="form-outline mb-4">
+                      <?php if(isset($dob_result)){echo $dob_result;}?>
+                        <input type="date" name="DOB" id="form3Example9" class="form-control form-control-lg" required />
+                        <label class="form-label" for="form3Example9">DOB</label>
+                      </div>
+      
+                      <div class="form-outline mb-4">
+                      <?php if(isset($password_result)){echo $password_result;}?>
+                        <input type="password" name="signUpPassword" id="form3Example90" class="form-control form-control-lg" required />
+                        <label class="form-label" for="form3Example90">password</label>
+                      </div>
+      
+                      <div class="form-outline mb-4">
+                      <?php if(isset($confirmPassword_result)){echo $confirmPassword_result;}?>
+                        <input type="password" name="signUpConfirmPassword" id="form3Example99" class="form-control form-control-lg" required />
+                        <label class="form-label" for="form3Example99">confirm password</label>
+                      </div>
+      
+                      <!-- <div class="form-outline mb-4">
+                        <input type="text" id="form3Example97" class="form-control form-control-lg" />
+                        <label class="form-label" for="form3Example97">Email ID</label>
+                      </div> -->
+      
+                      <div class="d-flex justify-content-end pt-3">
+                        <button type="reset" class="btn btn-light btn-lg">Reset all</button>
+                        <input type="submit" class="btn btn-warning btn-lg ms-2" name="submit" value="submit">
+                      </div>
+      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      </form>
+
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,128 @@
+<?php
+session_start();
+
+if (isset($_POST['submit'])){
+    
+    $_SESSION['loginEmail']=$_POST['loginEmail'];
+    $_SESSION['loginPassword']=$_POST['loginPassword'];
+    $adminEmail_correct=true;
+    $adminPass_correct=true;
+
+    foreach ($_SESSION['array'] as $key => $value) {
+        //Check Email
+        if($key == 'Email'){
+            if($_SESSION['loginEmail']==($value||'admin@gmail.com')){
+                $loginEmail_result="<span style=' color:green'>Correct Email</span><br>";
+                $loginEmail_correct=true;
+            }else{
+                $loginEmail_result="<span style=' color:red'>Incorrect Email</span><br>";
+                $loginEmail_correct=false;
+            }
+        }
+        //Check Password
+        if($key == 'Password Confirmation'){
+            if($_SESSION['loginPassword']==$value){
+                $loginPassword_result="<span style=' color:green'>Correct Password</span><br>";
+                $loginPassword_correct=true;
+            }else{
+                $loginPassword_result="<span style=' color:red'>Incorrect Password</span><br>";
+                $loginPassword_correct=false;
+            }
+        }
+    }
+    if($loginEmail_correct && $loginPassword_correct)
+        header('location:welcome.php');
+    else
+    echo '<script language="javascript">';
+    echo 'alert("Incorrect Information")'; 
+    echo '</script>';
+     
+    // cHeck Admin information 
+    if($_SESSION['loginEmail']=="admin@gmail.com"){
+		if($_SESSION['loginPassword']== "Admin*1234"){
+            $loginEmail_result="<span style=' color:green'>Correct Email</span><br>";
+			$adminEmail_correct=true;
+			$adminPass_correct=true;
+	
+		}else{
+			$loginPassword_result="<span style=' color:red'>Incorrect Password</span><br>";
+	    	$adminPass_correct=false;
+		}
+	}else{
+		$loginEmail_result="<span style=' color:red'>Incorrect Email</span><br>";
+		$adminEmail_correct=false;
+	}
+	if ($adminEmail_correct && $adminPass_correct ){
+		header('location:admin.php');
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+   <!--Made with love by Mutiullah Samim -->
+   
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Sign In</h3>
+				<!-- <div class="d-flex justify-content-end social_icon">
+					<span><i class="fab fa-facebook-square"></i></span>
+					<span><i class="fab fa-google-plus-square"></i></span>
+					<span><i class="fab fa-twitter-square"></i></span>
+				</div> -->
+			</div>
+			<div class="card-body">
+				<form method="post">
+                <?php if(isset($loginEmail_result)){echo $loginEmail_result;}?>
+
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+                        
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" name="loginEmail" class="form-control" placeholder="E-mail" required>
+						
+					</div>
+                    <?php if(isset($loginPassword_result)){echo $loginPassword_result;}?>
+
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" name="loginPassword" class="form-control" placeholder="password" required>
+					</div>
+                    <br>
+					<div class="form-group  ">
+                    <!-- <input type="submit" class="btn btn-warning btn-lg ms-2" name="submit" value="submit"> -->
+						<input type="submit" value="Login" name="submit" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					Don't have an account?<a href="Register.php">Sign Up</a>
+				</div>
+				<!-- <div class="d-flex justify-content-center">
+					<a href="#">Forgot your password?</a>
+				</div> -->
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
